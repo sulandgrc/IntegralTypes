c0fe077b304b50967421d888508e979e0cc30942
employee <suland@greenriver.edu>
2021-10-04 15:03:46 -0700

    Added comments and more descriptive output

--- src/primitives/IntegerClasses.java
+++ src/primitives/IntegerClasses.java
@@ -1,10 +1,17 @@
 package primitives;
 
+/**
+ * This class demos the integral data types available in Java and how to assign
+ * binary(0b) and hex(0x) values to the data types as well as access the constants defined in the
+ * various Integral Classes.
+ *
+ * author: Susan Uland
+ *
+ */
 public class IntegerClasses {
 
     public static void main(String[] args) {
         assignBinary();
-
 
     }
 
@@ -12,25 +19,26 @@ public class IntegerClasses {
         // integral types
         // byte holds 8 bits
         byte byteOne = 0b0101_1111;
-        System.out.println(byteOne);
+        System.out.println("Byte Decimal Value: " + byteOne);
 
         // 16 bits in a short
         short shortOne = 0b0111_0000_1111_1010;
-        System.out.println(shortOne);
+        System.out.println("Short Decimal Value: " + shortOne);
+        System.out.println();
 
         // 32 bits in an int
-        int intOne = 0b0101;
-        System.out.println(intOne);
+        int intOne = 0b0101_1111_0101_1110_0000_0000_0000_0101;
+        System.out.println("Integer Decimal Value: " + intOne);
+        System.out.println();
 
         // 64 bit in a long
-        long longOne = 0b1000_000_000_0000L;
-        System.out.println(longOne);
+        long longOne = 0b0101_1111_0101_1110_0000_0000_0000_0101_0101_1111_0101_1110_0000_0000_0000_0101L;
+        System.out.println("Long Decimal Value: " + longOne);
 
         // how many bits are in a Long?
         System.out.println("Num bits in Long: " + Long.SIZE);
-        System.out.println("Min: " + Long.MIN_VALUE);
-        System.out.println("Max: " + Long.MAX_VALUE);
-
+        System.out.println("Long Min: " + Long.MIN_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
 
     }
 

3e4e0425ef8e46864dc2b3c92b4bf59490f2a25c
employee <suland@greenriver.edu>
2021-10-06 16:23:12 -0700

    Added comments and more descriptive output

--- src/primitives/IntegerClasses.java
+++ src/primitives/IntegerClasses.java
@@ -11,7 +11,18 @@ package primitives;
 public class IntegerClasses {
 
     public static void main(String[] args) {
-        assignBinary();
+        // assignBinary();
+        // assignHex();
+
+        // check our answers
+        int num = 0b0010_0001;
+        System.out.println("Decimal Value: " + num);
+        System.out.println("Binary Value: " + Integer.toBinaryString(num));
+
+        System.out.println();
+        int num2 = 0x1A;
+        System.out.println("Decimal value: " + num2);
+        System.out.println("Binary Value: " + Integer.toBinaryString(num2));
 
     }
 
@@ -31,18 +42,40 @@ public class IntegerClasses {
         System.out.println("Integer Decimal Value: " + intOne);
         System.out.println();
 
-        // 64 bit in a long
+        // 64 bits in a long
         long longOne = 0b0101_1111_0101_1110_0000_0000_0000_0101_0101_1111_0101_1110_0000_0000_0000_0101L;
         System.out.println("Long Decimal Value: " + longOne);
 
         // how many bits are in a Long?
         System.out.println("Num bits in Long: " + Long.SIZE);
+
+        // what is the smallest value a Long can hold?
         System.out.println("Long Min: " + Long.MIN_VALUE);
+
+        // what is the largest value a Long can hold?
         System.out.println("Long Max: " + Long.MAX_VALUE);
 
     }
 
     public static void assignHex(){
+        // integral types
+        // byte holds 8 bits
+        // declare a byte and assigned a hex value
+        byte byteOne = 0x2A;
+        // (2 x 16 ^ 1) + (10  x 16 ^ 0) = 42
+        System.out.println("Decimal Value :" + byteOne);
+
+        // int, long have a toBinaryString() static method in their classes
+        // declare a short and assign a hex value to it
+        short num = 0x2FFF;
+        // (2 x 16 ^ 3) + (15 x 16 ^ 2) + (15 x 16 ^ 1) + (15 x 16 ^ 0) = 12,287
+        // print out the decimal value
+        System.out.println("Decimal Value: " + num);
+
+        // 32 bits every 4 bits represents one hex digit (0 - F)
+        int num2 = 0x206A_FFFF;
+        System.out.println("Binary Value: " + Integer.toBinaryString(num2));
+        System.out.println("Decimal Value: " + num2);
 
     }
 }
